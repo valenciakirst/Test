ed8a944d599b22f7854e1cc7c59423d0372c134e
valenciakirst <candykaye87@gmail.com>
2023-10-11 16:53:02 +0800

    new upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$int_var = 12;
+$another_int = 13 - 7;
+
+$many = 2.4444444;
+$many_2 = 4.5555555555;
+$few = $many + $int_var;
+print($many + $another_int = $few);
+
+<br>;
+
+echo "sanrio hello kitty";
+
+echo "sanrio hello ";
+
+?>
